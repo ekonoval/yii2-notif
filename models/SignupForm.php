67478537ea5e9c916f40ffc44e1e515343a51536
Yehor Konoval <ekonoval@gmail.com>
2016-04-04 21:23:57 +0300

    signup finish

--- models/SignupForm.php
+++ models/SignupForm.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use app\ext\User\UserIdentity;
 use Yii;
 use yii\base\Model;
 
@@ -22,13 +23,20 @@ class SignupForm extends User
         ];
     }
 
-    /**
-     * Logs in a user using the provided username and password.
-     * @return boolean whether the user is logged in successfully
-     */
     public function signup()
     {
         if ($this->validate()) {
+            $this->auth_key = Yii::$app->security->generateRandomString(32);
+            $this->password_hash = Yii::$app->security->generatePasswordHash($this->password);
+            $this->status = self::STATUS_ACTIVE;
+
+            if ($this->save()) {
+                Yii::$app->user->login(UserIdentity::findIdentity($this->primaryKey));
+
+                $auth = Yii::$app->authManager;
+                $auth->assign($auth->getRole(UserIdentity::ROLE_USER), $this->primaryKey);
+            }
+
             return true;
         }
         return false;
