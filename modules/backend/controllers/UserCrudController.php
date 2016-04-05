08c8aa2232401f2468dcbc8d56524585a9ba1fa1
Yehor Konoval <ekonoval@gmail.com>
2016-04-05 12:24:45 +0300

    users list

--- modules/backend/controllers/UserCrudController.php
+++ modules/backend/controllers/UserCrudController.php
@@ -2,20 +2,20 @@
 
 namespace app\modules\backend\controllers;
 
-use yii\web\Controller;
+use app\controllers\BackendController;
+use app\modules\backend\models\UserCrud\UserSearch;
 
-/**
- * Default controller for the `Backend` module
- */
-class UserCrudController extends Controller
+class UserCrudController extends BackendController
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
+
     public function actionIndex()
     {
-        echo "<h2>Vasya   </h2>\n";
-//        return $this->render('index');
+        $searchModel = new UserSearch();
+        $dataProvider = $searchModel->search(\Yii::$app->request->post());
+
+        return $this->render('index_tpl', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 }
