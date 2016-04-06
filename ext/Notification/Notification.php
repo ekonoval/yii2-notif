6516f57c76ec11d2eb4c31c2eef2e7854633f8e5
Yehor Konoval <ekonoval@gmail.com>
2016-04-06 10:07:04 +0300

    notification

---
+++ ext/Notification/Notification.php
@@ -0,0 +1,10 @@
+<?php
+namespace ext\Notification;
+
+class Notification
+{
+    const EVENT_USER_REGISTERED = 'EVENT_USER_REGISTERED';
+    const EVENT_USER_BLOCKED = 'EVENT_USER_BLOCKED';
+
+    const EVENT_ARTICLE_CREATED = 'EVENT_ARTICLE_CREATED';
+}
--- modules/backend/models/UserCrud/UserCrudSave.php
+++ modules/backend/models/UserCrud/UserCrudSave.php
@@ -1,9 +1,30 @@
 <?php
 namespace app\modules\backend\models\UserCrud;
 
+use app\ext\User\UserIdentity;
 use app\models\User;
+use ext\Notification\Notification;
+use yii\base\Event;
 
 class UserCrudSave extends User
 {
+    private $oldStatus;
+
+    public function afterFind()
+    {
+        parent::afterFind();
+
+        $this->oldStatus = $this->status;
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        if ($this->status == UserIdentity::STATUS_BLOCKED && $this->oldStatus != $this->status) {
+            $this->trigger(Notification::EVENT_USER_BLOCKED);
+        }
+    }
+
 
 }
