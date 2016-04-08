0f920a91a4988fa219a64274b8aaf297f9be1791
Yehor Konoval <ekonoval@gmail.com>
2016-04-08 10:16:04 +0300

    notif model

--- ext/Notification/Notification.php
+++ ext/Notification/Notification.php
@@ -3,8 +3,15 @@ namespace ext\Notification;
 
 class Notification
 {
-    const EVENT_USER_REGISTERED = 'EVENT_USER_REGISTERED';
-    const EVENT_USER_BLOCKED = 'EVENT_USER_BLOCKED';
+//    const EVENT_USER_REGISTERED = 'EVENT_USER_REGISTERED';
+//    const EVENT_USER_BLOCKED = 'EVENT_USER_BLOCKED';
+//    const EVENT_ARTICLE_CREATED = 'EVENT_ARTICLE_CREATED';
+//
+//    const TYPE_EMAIL = 1;
+//    const TYPE_BROWSER = 2;
+//
+//    const RECEIVER_ALL_ID = -1;
+//    const RECEIVER_OWNER_ID = -2;
 
-    const EVENT_ARTICLE_CREATED = 'EVENT_ARTICLE_CREATED';
+
 }
