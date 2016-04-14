f9e195fe95632a7a59cb7f8bfa59455086f7dd78
Yehor Konoval <ekonoval@gmail.com>
2016-04-14 11:09:12 +0300

    article event

--- modules/backend/models/ArticleCrud/ArticleCrudSave.php
+++ modules/backend/models/ArticleCrud/ArticleCrudSave.php
@@ -1,9 +1,23 @@
 <?php
 namespace app\modules\backend\models\ArticleCrud;
 
+use app\ext\Notification\NfBehavior;
 use app\models\Article;
+use app\models\Notification;
 
 class ArticleCrudSave extends Article
 {
+    public function init()
+    {
+        parent::init();
+        $this->attachBehavior(NfBehavior::NAME, NfBehavior::class);
+    }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+        $this->trigger(Notification::EVENT_ARTICLE_CREATED);
+    }
+
 
 }
