d69b199a5d510f2a289291c047eca1e4668a8410
Yehor Konoval <ekonoval@gmail.com>
2016-04-20 19:48:56 +0300

    add faker , filling CRMOperatorSteps

--- tests/acceptance/QueryCustomerByPhoneNumberCept.php
+++ tests/acceptance/QueryCustomerByPhoneNumberCept.php
@@ -6,9 +6,12 @@ use Step\Acceptance\CRMUserSteps;
 $I = new CRMOperatorSteps($scenario);
 $I->wantTo('add two different customers to database');
 $I->amInAddCustomerUi();
+
 $first_customer = $I->imagineCustomer();
+
 $I->fillCustomerDataForm($first_customer);
 $I->submitCustomerDataForm();
+
 $I->seeIAmInListCustomersUi();
 $I->amInAddCustomerUi();
 $second_customer = $I->imagineCustomer();
